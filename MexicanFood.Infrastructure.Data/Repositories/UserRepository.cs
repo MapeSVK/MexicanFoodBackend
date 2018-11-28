e72bfe6404123c4055e023eeef627b26b4a158d5
seelk89 <seelk.89@gmail.com>
2018-11-28 11:27:16 +0100

    minor issue

--- MexicanFood.Infrastructure.Data/Repositories/UserRepository.cs
+++ MexicanFood.Infrastructure.Data/Repositories/UserRepository.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using MexicanFood.Core.DomainService;
 using MexicanFood.Entities;
 
@@ -5,6 +6,29 @@ namespace MexicanFood.Infrastructure.Data.Repositories
 {
 	public class UserRepository : IRepository<User>
 	{
-		
+		public IEnumerable<User> ReadAllEntities()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public User EntityFoundById(int id)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public User CreateEntity(User entity)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public User UpdateEntity(int id, User entity)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public User DeleteEntity(int id)
+		{
+			throw new System.NotImplementedException();
+		}
 	}
 }
