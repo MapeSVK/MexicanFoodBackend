9efdeb07f0256c6ce037bdb616fcf6f98326a4a5
Tomasz Pleśniak <toma1777@easv365.dk>
2018-12-10 13:51:19 +0100

    Auto stash before merge of "Developer" and "origin/Developer"

--- MexicanFood.RestApi/Startup.cs
+++ MexicanFood.RestApi/Startup.cs
@@ -15,6 +15,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -117,6 +118,9 @@ namespace MexicanFood.RestApi
                 {
                     var ctx = scope.ServiceProvider.GetService<MexicanFoodContext>();
                     ctx.Database.EnsureCreated();
+	                var dbInitializer = ctx.GetService<IDBInitializer>();
+	                dbInitializer.SeedDb(ctx);
+	                
                 }
 
 				app.UseHsts();
